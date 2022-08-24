24ad52a343469ae4fb4d61e3be4e3824b6a6421a
smalltimebenny <ben.lindquist@gmail.com>
2022-08-24 15:36:34 -0400

    first commit

---
+++ FizzBuzz.js
@@ -0,0 +1,13 @@
+for (let x = 1; x < 101; x++){
+if (x % 3 == 0 && x % 5 == 0){
+    console.log ("FizzBuzz")
+} else if (x % 5 == 0){
+    console.log ("Buzz")
+} else if (x % 3 == 0) {
+    console.log ("Fizz")
+} else {
+    console.log(x)
+}
+
+
+}
